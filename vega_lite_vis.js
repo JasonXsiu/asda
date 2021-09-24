7c478ad6b591c6721828246b7ae37a55b7f67fef
JasonXsiu <sjsa3a28@gmail.com>
2021-09-24 12:05:20 +1000

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,36 @@
+var vg_eg = "econ_growth.vega.json";
+var vg_trade = "trade.vega.json";
+var vg_investment = "investment.vega.json";
+var vg_sector_gdp = "sector_gdp.vega.json";
+var vg_pvty_map = "pvty_map.vega.json";
+var vg_trade_cop = "trade copy.vega.json";
+var vg_pop_fert_life = "pop_fert_life.vega.json";
+
+
+vegaEmbed("#econ_growth", vg_eg).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#trade", vg_trade).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#investment", vg_investment).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#sector_gdp", vg_sector_gdp).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#pvty_map", vg_pvty_map).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#trade_copy", vg_trade_cop).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#pop_fert_life", vg_pop_fert_life).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
